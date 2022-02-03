198ca871cff15af27cc964be4312da57303b8205
bindash <ides.loup@gmail.com>
2022-02-03 13:49:35 +0100

    Navbar and Sidebar edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'index', component: IndexComponent},
+  {path: '**', redirectTo: 'index'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
